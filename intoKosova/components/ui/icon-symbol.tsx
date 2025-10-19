aa19bd770a60e82434a2bcc9af86d4910507fb37
DiellartMulolli <diellart.mulolli@student.uni-pr.edu>
2025-10-19 14:02:19 +0200

    added settings and made create better

--- intoKosova/components/ui/icon-symbol.tsx
+++ intoKosova/components/ui/icon-symbol.tsx
@@ -1,23 +1,23 @@
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { SymbolWeight, SymbolViewProps } from 'expo-symbols';
 import { ComponentProps } from 'react';
 import { OpaqueColorValue, type StyleProp, type TextStyle } from 'react-native';
 
-type IconMapping = Record<SymbolViewProps['name'], ComponentProps<typeof MaterialIcons>['name']>;
+type IconMapping = Record<SymbolViewProps['name'], ComponentProps<typeof Ionicons>['name']>;
 type IconSymbolName = keyof typeof MAPPING;
-
 
 const MAPPING = {
   'house.fill': 'home',
-  'paperplane.fill': 'send',
+  'paperplane.fill': 'paper-plane',
   'chevron.left.forwardslash.chevron.right': 'code',
-  'chevron.right': 'chevron-right',
+  'chevron.right': 'chevron-forward',
   'person.fill': 'person',
-  'info.circle': 'info',
+  'info.circle': 'information-circle',
   'magnifyingglass': 'search',
   'list.bullet': 'list',
+  'gearshape.fill': 'settings',
+  'circled.plus': 'add-circle', // Maps to ⊕︀ (circled plus)
 } as IconMapping;
-
 
 export function IconSymbol({
   name,
@@ -31,5 +31,5 @@ export function IconSymbol({
   style?: StyleProp<TextStyle>;
   weight?: SymbolWeight;
 }) {
-  return <MaterialIcons color={color} size={size} name={MAPPING[name]} style={style} />;
+  return <Ionicons color={color} size={size} name={MAPPING[name]} style={style} />;
 }
