c80f5a7352aea75a483612f4f3aa766a3c617207
ShpetimPanduri <shpetim.panduri@student.uni-pr.edu>
2025-11-12 15:15:28 +0100

    Firebase connected

---
+++ intoKosova/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuvn_ZyvhGqnnJH1g39pNwQoRaEo6Lzo4",
+  authDomain: "intokosova.firebaseapp.com",
+  projectId: "intokosova",
+  storageBucket: "intokosova.firebasestorage.app",
+  messagingSenderId: "1063249578114",
+  appId: "1:1063249578114:web:55ce1b1352c24e77f80c9b"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
